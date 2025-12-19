3e2f0c4ff2bdec4ff5493a48d71900e47e6d39a4
sofiaorfa <sofiaorfanou14@gmail.com>
2025-12-19 13:52:00 +0200

    Update peaks.sh

--- scripts/peaks.sh
+++ scripts/peaks.sh
@@ -4,7 +4,8 @@
 for bg in *.bedGraph
 do
     base=$(basename "$bg" .bedGraph)
-    awk '$4 > 5 {print $1,$2,$3,$4}' OFS="\t" "$bg" > "${base}_peaks.bed"
+    awk '$4 > 5 {print $1,$2,$3,$4}' OFS="\t" "$bg" > "${base}_peaks.bed"     # Keep only regions where the 4th column (signal) > 5
 done
 
 echo "Peaks called for all .bedGraph files"
+
